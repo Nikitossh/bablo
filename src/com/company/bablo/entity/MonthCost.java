fa227a45b4842bb7ee7763858863f7d1b2297e6c
Nikita Shesterikov <nikitossh@mail.ru>
2018-11-09 09:53:43 +0300

    Added ENUM Months with all month

--- src/com/company/bablo/entity/MonthCost.java
+++ src/com/company/bablo/entity/MonthCost.java
@@ -5,19 +5,19 @@ import java.sql.SQLException;
 import java.util.*;
 
 import static com.company.bablo.persistent.DAO.selectionMonth;
-import static com.company.bablo.persistent.DAO.selectionThisMonth;
 
 /** Этот класс соде */
+//todo: Сломается когда будут разные категории в мапах
 
 public class MonthCost {
-    public Map<String, Integer> getMap() {
-        return map;
-    }
 
+    // В мапе хранится пара категория-сумма, месяц и год для идентификации
     private Map<String, Integer> map;
     private int month;
     private int year;
 
+    // При создании класса указываем месяц и год, метод getMonthCost() заполняет мапу
+    // todo: будет падать если такого месяца еще нет
     public MonthCost(int month, int year) {
         this.month = month;
         this.year = year;
@@ -28,6 +28,7 @@ public class MonthCost {
         }
     }
 
+    // Заполняем мапу исходя из значения месяца, указанного при создании объекта
     private Map<String, Integer> getMonthCost() throws SQLException {
         Map<String, Integer> map = new HashMap<>();
         ResultSet rsCategory = selectionMonth(month);
@@ -39,19 +40,22 @@ public class MonthCost {
 
     public void printTable(ArrayList<MonthCost> list) {
         Formatter f = new Formatter(System.out);
+        // Получаю все категории из первой же мапы
         Set<String> set = list.get(0).map.keySet();
+
         Iterator iterator = set.iterator();
 
         while (iterator.hasNext()) {
             String category = (String) iterator.next();
+            // Печатаю название категории
             f.format("%-20s", category);
-
+            // И значения из каждой мапы, название категории - ключ
             for (MonthCost m : list) {
-                f.format("%-15s ", m.map.get(category));
+                f.format("%-10s ", m.map.get(category));
             }
 
             System.out.println();
-    }
+        }
     }
 
     public static void main(String[] args) {
@@ -59,13 +63,15 @@ public class MonthCost {
         MonthCost jan = new MonthCost(1, 2018);
         MonthCost feb = new MonthCost(2, 2018);
         MonthCost mar = new MonthCost(3, 2018);
+        MonthCost apr = new MonthCost(4, 2018);
+        MonthCost may = new MonthCost(5, 2018);
         ArrayList<MonthCost> months = new ArrayList<>();
         months.add(jan);
         months.add(feb);
         months.add(mar);
+        months.add(apr);
+        months.add(may);
          year.printTable(months);
 
         }
-
-
 }
---
+++ src/com/company/bablo/entity/Monthes.java
@@ -0,0 +1,16 @@
+package com.company.bablo.entity;
+
+public enum Monthes {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
