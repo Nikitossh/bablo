3a9528f29ce95fc97539ea700ac4e1c96ca20db6
Nikita Shesterikov <nikitossh@mail.ru>
2018-11-12 18:04:13 +0300

    Moved print methods to separate class

--- src/com/company/bablo/entity/MonthCost.java
+++ src/com/company/bablo/entity/MonthCost.java
@@ -1,26 +1,21 @@
 package com.company.bablo.entity;
 
-import com.company.bablo.ConsoleView;
-import com.company.bablo.persistent.DAO;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.Month;
 import java.util.*;
 
-import static com.company.bablo.entity.Categories.getCategoriesSet;
-import static com.company.bablo.persistent.DAO.getCategoriesRS;
+import static com.company.bablo.Terminal.PrintMonthCost.printTable;
 import static com.company.bablo.persistent.DAO.selectionMonth;
-import static com.company.bablo.persistent.DAO.selectionTotalValuesMonth;
 
 public class MonthCost {
-
     // В мапе хранится пара категория-сумма, месяц и год для идентификации
     private Map<String, Integer> map;
     private int month;
     private int year;
     private String name;
-    private static Formatter f = new Formatter(System.out);
+
 
 
     // При создании класса указываем месяц и год, метод getMonthCost() заполняет мапу
@@ -45,58 +40,6 @@ public class MonthCost {
         return map;
     }
 
-    public static void printMonthes(ArrayList<MonthCost> list) {
-        f.format("%-20s", "Categories");
-        for (int i = 0; i < list.size(); i++) {
-            f.format("%-12s", list.get(i).name);
-        }
-        System.out.println();
-    }
-
-    public static void printSeparators(ArrayList<MonthCost> list) {
-        f.format("%-20s", "---------");
-        for (int i = 0; i < list.size(); i++) {
-            f.format("%-12s", "--------");
-        }
-        System.out.println();
-    }
-
-    public static void printTotal(ArrayList<MonthCost> list) throws SQLException {
-        f.format("%-20s", "Total:");
-        for (int i = 0; i < list.size(); i++) {
-            ResultSet rs = selectionTotalValuesMonth(list.get(i).month, list.get(i).year);
-            while (rs.next())
-                f.format("%-12s", rs.getString(1));
-        }
-    }
-
-    public static void printTable(ArrayList<MonthCost> list) throws SQLException {
-        // Печатаю шапку таблицы
-        printMonthes(list);
-        printSeparators(list);
-
-        // Получаю все категории в виде TreeSet
-        Set<String> set = getCategoriesSet(getCategoriesRS());
-        Iterator iterator = set.iterator();
-        while (iterator.hasNext()) {
-            String category = (String) iterator.next();
-            // Печатаю категории
-            f.format("%-20s", category);
-            // И значения из каждой мапы, название категории - ключ
-            for (MonthCost m : list) {
-                f.format("%-12s", m.map.get(category));
-            }
-
-            System.out.println();
-        }
-
-        // Печатаю total за месяцы
-        printSeparators(list);
-        printTotal(list);
-    }
-
-
-
     public static ArrayList<MonthCost> getYear(int year) {
         ArrayList<MonthCost> theYear = new ArrayList<>();
         for (int i = 1; i < Month.values().length; i++) {
@@ -112,5 +55,21 @@ public class MonthCost {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<String, Integer> getMap() {
+        return map;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
     }
 }
